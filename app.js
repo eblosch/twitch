80a62800bf2e6149f982c2664756f0bd22d9a3e8
Eric Blosch <eblosch@706media.com>
2015-07-03 21:34:23 -0700

    First pass at JSON jquery implementation

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var app = angular.module('TwitchApp', []);
-var url = "https://api.twitch.tv/kraken/streams/freecodecamp";
+var url = "https://api.twitch.tv/kraken/streams/freecodecamp?callback=?";
 // var url = "http://www.w3schools.com/angular/customers.php";
 
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
-    $http.get(url).
-        success(function(data) {
-            $scope.data = data;
-        }).
-        error(function(err) {
-            $scope.error = 'No data received';
-        });
+
+//    var streams = ['freecodecamp', 'medrybw'];
+//    $scope.streams = streams;
+
+    // $http.get(url).
+    //     success(function(data, status) {
+    //         $scope.data = data;
+    //         $scope.status = status;
+    //     }).
+    //     error(function(err) {
+    //         $scope.error = 'No data received';
+    //     });
 }]);
